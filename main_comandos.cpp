9768f1ec837731a73365b64be3213bada6f870be
Vitojgs <106398413+Vitojgs@users.noreply.github.com>
2026-04-19 21:31:50 +0100

    Delete main_comandos.cpp

--- main_comandos.cpp
+++
@@ -1,5 +0,0 @@
-//   g++ main.cpp lib/vc.c -o main -I"C:/msys64/mingw64/include/opencv4" -L"C:/msys64/mingw64/lib" -lopencv_core -lopencv_highgui -lopencv_videoio -lopencv_imgproc -std=c++17 -lm
-
-//        .\CitrID.exe
-
-
